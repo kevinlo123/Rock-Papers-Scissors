184db067cbafa88d19837ead600377ae0f5678bd
Your Name <you@example.com>
2017-12-18 19:36:29 -0500

    tab change

--- rps.js
+++ rps.js
@@ -1,49 +1,49 @@
 //Author: Kevin Lopez
 
-let userPoints = 0;
-let cpuPoints = 0;
+let userPoints = 0; //user points 
+let cpuPoints = 0; //cpu points
 const rockElement = document.getElementById("rock");
 const paperElement = document.getElementById("paper");
 const scissorsElement = document.getElementById("scissors");
 
 
 const userChoseRock = () => {
-  	const botsWeapon = computerThrows();
-  	const usersWeapon = "rock";
-  	checkWhoWon(botsWeapon , usersWeapon);
+  const botsWeapon = computerThrows();
+  const usersWeapon = "rock";
+  checkWhoWon(botsWeapon , usersWeapon);
 }
 
 const userChosePaper = () => {
-  	const botsWeapon = computerThrows();
-  	const usersWeapon = "paper";
-  	checkWhoWon(botsWeapon , usersWeapon);
+  const botsWeapon = computerThrows();
+  const usersWeapon = "paper";
+  checkWhoWon(botsWeapon , usersWeapon);
 }
 
 const userChoseScissors = () =>{
-  	const botsWeapon = computerThrows();
-  	const usersWeapon = "scissors";
-  	checkWhoWon(botsWeapon , usersWeapon);
+  const botsWeapon = computerThrows();
+  const usersWeapon = "scissors";
+  checkWhoWon(botsWeapon , usersWeapon);
 }
 
 const addPointsToPlayer = () => {
-  	userPoints += 1;
-  	document.getElementsByTagName("span")[0].innerHTML = userPoints;
+  userPoints += 1;
+  document.getElementsByTagName("span")[0].innerHTML = userPoints;
 }
 
 const addPointsToCpu = () => {
-  	cpuPoints += 1;
-  	document.getElementsByTagName("span")[1].innerHTML = cpuPoints;
+  cpuPoints += 1;
+  document.getElementsByTagName("span")[1].innerHTML = cpuPoints;
 }
 
 const computerThrows = () => {
-  	let randomNumber = Math.random();
-  	let botsWeapon = "rock";
-  	if(randomNumber < .33){
-  		botsWeapon = "paper";
-  	}else if(randomNumber > .66){
-    	botsWeapon = "scissors";
-  	}
-  	return botsWeapon;
+  let randomNumber = Math.random();
+  let botsWeapon = "rock";
+  if(randomNumber < .33){
+  	botsWeapon = "paper";
+  }else if(randomNumber > .66){
+    botsWeapon = "scissors";
+  }
+  return botsWeapon;
 }
 
 const checkWhoWon = (botsweapon , usersWeapon) => {
